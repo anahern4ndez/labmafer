16098f75dfa01ae0864dd3303c90c240de78bae9
diazMafer <31084398+diazMafer@users.noreply.github.com>
2017-11-03 09:08:01 -0600

    Region

---
+++ Laboratorio final/src/laboratorio/pkgfinal/Region.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package laboratorio.pkgfinal;
+
+import java.util.Date;
+
+/**
+ *
+ * @author Mafer
+ */
+public class Region {
+    //Atributos 
+    private String descripcion;
+    private Date fecha;
+    
+    public Region(String descripcion, Date fecha){
+        this.descripcion=descripcion;
+        
+    }
+    
+    
+}
